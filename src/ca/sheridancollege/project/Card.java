5e5bdf807063adb2a567ac913490a0743af552b4
Tibabalase Oludemi <30732454+lafftar@users.noreply.github.com>
2020-06-12 13:38:56 -0400

    Bryan Acorda Merge (#1)
    * Just added the date and my name under Author to each class
    
    * Made a UML Diagram for the project.
    
    * Made a UML Diagram for the project.
    
    Co-authored-by: bryanacorda <acordab@sheridancollege.ca>

---
+++ src/ca/sheridancollege/project/Card.java
@@ -0,0 +1,27 @@
+/**
+ * SYST 17796 Project Base code.
+ * 
+ * Date: 06-09-2020
+ * @Author Bryan Acorda
+ * Group 3
+ */
+package ca.sheridancollege.project;
+
+/**
+ * A class to be used as the base Card class for the project. Must be general enough to be instantiated for any Card
+ * game. Students wishing to add to the code should remember to add themselves as a modifier.
+ *
+ * @author dancye
+ */
+public abstract class Card {
+    //default modifier for child classes
+
+    /**
+     * Students should implement this method for their specific children classes
+     *
+     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
+     */
+    @Override
+    public abstract String toString();
+
+}
