7303c463ecea20900a015328412ca7e17fbdd5eb
asun975 <65915716+asun975@users.noreply.github.com>
2020-08-08 15:50:28 -0400

    added string to suit enum

--- src/ca/sheridancollege/project/Suit.java
+++ src/ca/sheridancollege/project/Suit.java
@@ -5,12 +5,13 @@ package ca.sheridancollege.project;
  * Enum for the suit of a war card. displaySuit : String, a string
  * representation of the suit i.e., "Spades"
  * Created enum
+ * @author Ashley Sun
  */
 public enum Suit {
-    CLUBS,
-    HEARTS,
-    DIAMONDS,
-    SPADES;
+    CLUBS("Clubs"),
+    HEARTS ("Hearts"),
+    DIAMONDS ("Diamonds"),
+    SPADES ("Spades");
 
     private String displaySuit;
 
@@ -23,8 +24,7 @@ public enum Suit {
      * @param suit
      */
     private Suit(String suit) {
-        // TODO - implement Suit.Suit
-        throw new UnsupportedOperationException();
+        displayeSuit = suit;
     }
 
 }
