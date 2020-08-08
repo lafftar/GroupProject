f087e84b4d64fe2bd1bb2fe604d987fc586481f5
Purv267 <patpurvr@sheridancollege.ca>
2020-08-07 21:38:06 -0400

    Created a String data member namely player1 and player2. Added their getter and setter for that data members.

--- src/ca/sheridancollege/project/WarPlayers.java
+++ src/ca/sheridancollege/project/WarPlayers.java
@@ -10,28 +10,30 @@ package ca.sheridancollege.project;
  * @author purvp
  */
 public class WarPlayers extends Player {
-    private Player player1;
-    private Player player2;
-    public WarPlayers(String name) {
+    private String player1;
+    private String player2;
+
+    public WarPlayers(String player1, String player2, String name) {
         super(name);
+        this.player1 = player1;
+        this.player2 = player2;
     }
 
-    public Player getPlayer1() {
+    public String getPlayer1() {
         return player1;
     }
 
-    public void setPlayer1(Player player1) {
+    public void setPlayer1(String player1) {
         this.player1 = player1;
     }
 
-    public Player getPlayer2() {
+    public String getPlayer2() {
         return player2;
     }
 
-    public void setPlayer2(Player player2) {
+    public void setPlayer2(String player2) {
         this.player2 = player2;
     }
-    
 
     @Override
     public void play() {
