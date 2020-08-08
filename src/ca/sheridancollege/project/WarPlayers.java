d1d3012aa4426d521b7bced1a81d2480ad497054
Purv267 <patpurvr@sheridancollege.ca>
2020-08-07 21:33:51 -0400

    Created a WarPlayers class extends abstract class Player. Abstract keyword is not removed.

---
+++ src/ca/sheridancollege/project/WarPlayers.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.project;
+
+/**
+ *
+ * @author purvp
+ */
+public class WarPlayers extends Player {
+    private Player player1;
+    private Player player2;
+    public WarPlayers(String name) {
+        super(name);
+    }
+
+    public Player getPlayer1() {
+        return player1;
+    }
+
+    public void setPlayer1(Player player1) {
+        this.player1 = player1;
+    }
+
+    public Player getPlayer2() {
+        return player2;
+    }
+
+    public void setPlayer2(Player player2) {
+        this.player2 = player2;
+    }
+    
+
+    @Override
+    public void play() {
+       
+    }
+    
+}
