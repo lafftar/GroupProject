d65ce7fdad1d1569a3cc4e1375d1417a48e4e0e5
lafftar <tibabalase@gmail.com>
2020-08-08 17:36:18 -0400

    Re-added WarPlayer to project structure

---
+++ src/ca/sheridancollege/project/WarPlayer.java
@@ -0,0 +1,83 @@
+package ca.sheridancollege.project;
+
+/**
+ * Models a player in the game War
+ *
+ */
+public class WarPlayer extends Player {
+
+    /**
+     * playerDeck - player's hand of cards
+     */
+    private PlayerDeck deck;
+    /**
+     * score - how many rounds the player won
+     */
+    private int score;
+    /**
+     * stats - how many games the player won
+     *
+     */
+    private double stats;
+
+    public PlayerDeck getDeck() {
+        return this.deck;
+    }
+
+    /**
+     *
+     * @param deck
+     */
+    public void setDeck(PlayerDeck deck) {
+        this.deck = deck;
+    }
+
+    /**
+     * returns a war card from the player's playerDeck arraylist
+     *
+     * @return
+     */
+    public WarCard drawCard() {
+        return new WarCard(Suit.CLUBS, Rank.EIGHT); // just for testing, delete.
+    }
+
+    public int getScore() {
+        return this.score;
+    }
+
+    /**
+     *
+     * @param score
+     */
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public double getStats() {
+        return this.stats;
+    }
+
+    /**
+     *
+     * @param stats
+     */
+    public void setStats(double stats) {
+        this.stats = stats;
+    }
+
+    public void calcStats() {
+        // TODO - implement WarPlayer.calcStats
+        throw new UnsupportedOperationException();
+    }
+
+    public void calcScore() {
+        // TODO - implement WarPlayer.calcScore
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void play() {
+
+    }
+
+}
