d1a154b30d79ac46e6b3cdfc0fcdc2cbc69310b3
asun975 <65915716+asun975@users.noreply.github.com>
2020-08-09 20:39:11 -0400

    updated toString()

--- src/ca/sheridancollege/project/WarCard.java
+++ src/ca/sheridancollege/project/WarCard.java
@@ -46,6 +46,6 @@ public class WarCard extends Card {
 
     @Override
     public String toString() {
-        return this.getRank() + " of " + this.getSuit();
+        return rank.getDisplayRank() + " of " + suit.getDisplaySuit();
     }
 }
