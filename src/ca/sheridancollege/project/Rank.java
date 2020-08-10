824e2222c7cf3f547e9ae9eec0306ffc4e25d924
bryanacorda <acordab@sheridancollege.ca>
2020-08-09 20:18:00 -0400

    Rank: - changed ACE as the largest card

--- src/ca/sheridancollege/project/Rank.java
+++ src/ca/sheridancollege/project/Rank.java
@@ -6,7 +6,7 @@ package ca.sheridancollege.project;
  * @author Ashley Sun
  */
 public enum Rank {
-    ONE("One", 1),
+    
     TWO("Two", 2),
     THREE("Three", 3),
     FOUR("Four", 4),
@@ -18,7 +18,8 @@ public enum Rank {
     TEN("Ten", 10),
     JACK("Jack", 11),
     QUEEN("Queen", 12),
-    KING("King", 13);
+    KING("King", 13),
+    ACE("ACE", 14);
 
     //user-friendly format of rank
     private String displayRank;
