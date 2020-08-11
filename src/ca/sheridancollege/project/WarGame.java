36a400bb3e29e18d4c69cd1a6e7bc129ffaf42f2
lafftar <tibabalase@gmail.com>
2020-08-11 02:27:29 -0400

    Fixed card bug with WarGame

--- src/ca/sheridancollege/project/WarGame.java
+++ src/ca/sheridancollege/project/WarGame.java
@@ -4,14 +4,14 @@ import java.util.*;
 import static java.util.Arrays.asList;
 
 public class WarGame extends Game {
-    
+
     private WarPlayer player1 = new WarPlayer("Player 1");
     private WarPlayer player2 = new WarPlayer("Player 2");
     private PlayerDeck placeholderDeck = new PlayerDeck(0); // deck to hold cards that are discarded in the war round
     ArrayList<WarPlayer> player = new ArrayList(asList(player1, player2)); // Store each player
     ArrayList<WarCard> playerCards;
     ; // Deck that holds cards that are discarded in the war round
-    
+
     // allows us to declare gameOver in warInitiated and have it carry over to the distributeWinnings() method
     private boolean gameOver = false;
     // allows us to stop the game after 100 rounds have been played, and declare the player with most cards the winner
@@ -21,7 +21,7 @@ public class WarGame extends Game {
     public WarGame(String name) {
         super(name);
     }
-    
+
     public WarPlayer getPlayer1() {
         return this.player1;
     }
@@ -33,7 +33,7 @@ public class WarGame extends Game {
     public void setPlayer1(WarPlayer player1) {
         this.player1 = player1;
     }
-    
+
     public WarPlayer getPlayer2() {
         return this.player2;
     }
@@ -45,7 +45,7 @@ public class WarGame extends Game {
     public void setPlayer2(WarPlayer player2) {
         this.player2 = player2;
     }
-    
+
     public PlayerDeck getWarDeck() {
         return this.placeholderDeck; // just assuming, confirm
     }
@@ -70,7 +70,6 @@ public class WarGame extends Game {
 //         1 if card 1 is greater
 //         2 if card 2 is greater
 //         3 if they're equal
-
         // Yeah... I changed these values to 0,1,2. This way the ArrayList for
         // distributeWinnings() method could work -Bryan
         if (card1.getRank().getRankNumber() > card2.getRank().getRankNumber()) {
@@ -81,7 +80,7 @@ public class WarGame extends Game {
         }
         return 2;
     }
-    
+
     private WarPlayer returnPlayerWithLowerCards() {
         if (player.get(0).getDeck().getSize() < player.get(1).getDeck().getSize()) {
             return player.get(0);
@@ -96,57 +95,49 @@ public class WarGame extends Game {
      * @return
      */
     public void warInitiated(WarCard p1Card, WarCard p2Card) {
-        
+
         // Store in array
         ArrayList<WarCard> cards = new ArrayList(asList(p1Card, p2Card));
-        
+        boolean atWar = this.compareCards(p1Card, p2Card) == 2;
+
         // while war is declared, we want the methods above to loop twice.
         // This loop will only be entered when war is declared from first play,
         // which is obvs the only time war can be declared
-        
         // if any player has less than two cards when war is initiated
-        
         // I think this is the reason why the game cannot finish, remember that
-        // the condition is that the games ends with a winner or loser if either  
+        // the condition is that the games ends with a winner or loser if either
         // the winner has all 52 cards OR or loser has no cards at all.
-        // With this, you are preventing a player to lose because you aren't 
+        // With this, you are preventing a player to lose because you aren't
         // letting the loser have less that 2 cards. -Bryan
-        if (player.get(0).getDeck().getSize() < 2 || player.get(1).getDeck().getSize() < 2) {
+        if ((player.get(0).getDeck().getSize() < 2 || player.get(1).getDeck().getSize() < 2) && atWar) {
             this.gameOver = true;
             System.out.println(String.format("War has been initiated, but Player '%s' has less than 2 cards! "
                     + "They cannot afford to go to war!", this.returnPlayerWithLowerCards().getName()));
         } else {
-            // Changed the comparing number to 2 -Bryan
-            while (this.compareCards(p1Card, p2Card) == 2) {
+            while (atWar) {
+                // Changed the comparing number to 2 -Bryan
                 System.out.println("War has been initiated!!!");
                 for (int i = 0; i < 2; i++) {
-                    
+
                     p1Card = player.get(0).drawCard();
                     p2Card = player.get(1).drawCard();
-                    
-                    if (i == 1) {
-                        // WarCard might need a better toString()
-                        for (WarPlayer warPlayer : player) {
-                            System.out.println(warPlayer + " drew: " + cards.get(player.indexOf(warPlayer)));
-                        }
-//                        System.out.println("Player 1 Drew: " + p1Card);
-//                        System.out.println("Player 2 Drew: " + p2Card);
 
-                    }
+                    System.out.println(String.format("Player '%s' drew %s", this.player1.getName(), p1Card));
+                    System.out.println(String.format("Player '%s' drew %s", this.player2.getName(), p2Card));
+
                     // take 1 card from  each player, add to placeholder deck
                     this.placeholderDeck.addCardToPlayerDeck(p1Card);
                     this.placeholderDeck.addCardToPlayerDeck(p2Card);
 
-                    // might benefit from having a remove card method for the player
                     // remove the card we just drew from each player
-
                     this.player.get(0).getDeck().getPlayerDeck().remove(p1Card);
                     this.player.get(1).getDeck().getPlayerDeck().remove(p2Card);
 
                     this.incrementNumRounds();
                 }
+                atWar = this.compareCards(p1Card, p2Card) == 2;
             }
-            
+
         }
     }
 
@@ -179,7 +170,7 @@ public class WarGame extends Game {
      * @param num
      */
     public void distributeWinnings(WarCard player1Card, WarCard player2Card, int num) {
-        
+
         if (!this.gameOver) {
             if (this.compareCards(player1Card, player2Card) == num) { // If a player won
                 // that player will have all cards in placeholder deck added to the bottom of his pile.
@@ -193,7 +184,7 @@ public class WarGame extends Game {
                 for (WarCard card : this.placeholderDeck.getCards()) {
                     player.get(num).getDeck().addCardToPlayerDeck(card);
                 }
-                
+
                 this.placeholderDeck.getCards().removeAll(player.get(num).getDeck().getCards());
                 // the .getSize() method only returns the size of the class at initialization, not while it's moving
                 // i've overridden the method in playerdeck
@@ -202,14 +193,14 @@ public class WarGame extends Game {
                 for (WarPlayer warPlayer : player) {
                     System.out.println(String.format(warPlayer + " Now has %d cards", warPlayer.getDeck().getSize()));
                 } // -Bryan
-                
+
 //                System.out.println(String.format(player.get(0).getName()
 //                        + " Now Has %d cards", this.player1.getDeck().getSize()));
 //                System.out.println(String.format(player.get(1).getName()
 //                        + " Now Has %d cards", this.player2.getDeck().getSize()));
             }
         }
-        
+
     }
 
     /**
@@ -219,63 +210,61 @@ public class WarGame extends Game {
      */
     @Override
     public void declareWinner() {
-        
+
         // Put these here to clean up the conditions a bit
         WarPlayer p1 = player.get(0);
         WarPlayer p2 = player.get(1);
         int p1Condition = p1.getDeck().getPlayerDeck().size();
         int p2Condition = p2.getDeck().getPlayerDeck().size();
-        
-        
+
         // changing this to declare winner on the person with the most cards
         // instead
         if (p1Condition > p2Condition) {
             System.out.println(p1.getName() + " Wins!");
             p1.setWins(p1.getWins() + 1);
             p2.setLosses(player.get(1).getLosses() + 1);
-            
+
         } else if (p1Condition == p2Condition) {
             System.out.println("Both players have the same number of cards! "
                     + "You both get a win!");
-            
+
             for (WarPlayer warPlayer : player) {
                 warPlayer.setWins(warPlayer.getWins() + 1);
             } // -Bryan
-            
+
 //            player.get(0).setWins(player.get(0).getWins() + 1);
 //            player.get(1).setWins(player.get(1).getWins() + 1);
         } else {
             System.out.println(p2.getName() + " Wins!");
             p2.setWins(p2.getWins() + 1);
             p1.setLosses(p1.getLosses() + 1);
-            
+
         }
 
         // Created a score ouput for each player, it doesn't work accurately as
         // this is just a test. -Bryan
-        String outputScore = "\nGame Stats:\n" + player1.getStats() + "\n" + player2.getStats();
+        String outputScore = String.format("\nGame Stats:\nPlayers : Wins : Losses\n%s : %s\n%s : %s",
+                this.player1.getName(), this.player1.getStats(), this.player2.getName(), this.player2.getStats());
         System.out.println(outputScore);
     }
-    
+
     /**
      * This method resets all values from each player
-     * 
+     *
      */
     private void resetGame() {
         this.gameOver = false;
         this.numRounds = 0;
-        for (WarPlayer warPlayer : player) {
+        for (WarPlayer warPlayer : this.player) {
             warPlayer.getDeck().wipePlayerDeck();
-        } // -Bryan
-//        this.player1.getDeck().wipePlayerDeck();
-//        this.player2.getDeck().wipePlayerDeck();
+        }
         this.placeholderDeck.wipePlayerDeck();
     }
-    
+
     /**
      * This method increments the rounds and checks and stops game after 1000
      * increments
-     * 
+     *
      */
     private void incrementNumRounds() {
         this.numRounds++;
@@ -308,24 +297,22 @@ public class WarGame extends Game {
 //            this.setPlayer2(new WarPlayer(in.nextLine()));
 //            System.out.println("Players:\n" + this.player1.getName()
 //                    + "\n" + this.player2.getName());
-
         // need to shuffle the deck and distribute the cards from wargroupofcards
         // first we create the wargroupofcards, it's created, assembled and shuffled in the wargroupofcards class
         WarGroupOfCards allCards = new WarGroupOfCards();
-        
+
         allCards.dealToPlayerDeck(new ArrayList<>(asList(this.player1, this.player2)));
         // this will need to be nested further, to have support for continious play
         while (this.player.get(0).getDeck().getSize() > 0 && this.player.get(1).getDeck().getSize() > 0 && !this.gameOver) {
 
-             /**
-             OKAY SO there was a lot of DRY humping going on betweeen player1 
-             and player2.. gave them a room by storing them in arraylists 
-             and cleaned it up by printing out all repeating lines of code 
-             in for-each loops. Although I did leave all the repeated
-             code commented out just in case you wanted to bring them back
-             -Bryan
-             **/
-            
+            /**
+             * OKAY SO there was a lot of DRY humping going on betweeen player1
+             * and player2.. gave them a room by storing them in arraylists and
+             * cleaned it up by printing out all repeating lines of code in
+             * for-each loops. Although I did leave all the repeated code
+             * commented out just in case you wanted to bring them back -Bryan
+             *
+             */
             // start game
             // both players deal
             WarCard player1Card = player.get(0).drawCard();
@@ -343,7 +330,6 @@ public class WarGame extends Game {
 
 //            System.out.println(player1.getName() + " Drew: " + playerCards.get(0).toString());
 //            System.out.println(player2.getName() + " Drew: " + playerCards.get(1).toString());
-
             // deal the first cards,
             // place it in the placeholder deck
             // remove the cards from each players deck.
@@ -383,11 +369,5 @@ public class WarGame extends Game {
         declareWinner();
         // resets the game, wipes all playerdecks, sets gameOver to false.
         this.resetGame();
-    }
-    
-    public static void main(String[] args) {
-        WarGame game = new WarGame("War");
-        game.play();
-        
     }
 }
