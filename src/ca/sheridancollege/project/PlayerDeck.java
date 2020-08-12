3f3cb2a20ce4dae691c98a181cbdf65cc40e50b0
asun975 <65915716+asun975@users.noreply.github.com>
2020-08-11 20:21:05 -0400

    javadocs done
    everyone should review this before submission.

--- src/ca/sheridancollege/project/PlayerDeck.java
+++ src/ca/sheridancollege/project/PlayerDeck.java
@@ -4,21 +4,27 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 /**
- * This models the each individual player deck which store cards in an array,
- * has the ability to add cards, get cards from a specific index  and remove all
- * cards from the deck.
+ * This models the each individual player deck which store cards in an array.
+ * The PlayerDeck class has the ability to add cards, get cards from a specific 
+ * index  and remove all cards from the deck.
  * 
  * @author Bryan Acorda
  */
 public class PlayerDeck extends GroupOfCards {
 
     /**
-    * Constructs a GroupOfCards with 26 WarCards.
+    * Constructs a PlayerDeck object with 26 cards of type WarCard.
+    *
     */
     public PlayerDeck() {
         super(26);
     }
 
+    /**
+    * Constructs a Playerdeck arrayList with a programmer-specified size.
+    *
+    * @param size of arrayList
+    */
     public PlayerDeck(int size) {
         super(size);
     }
@@ -34,6 +40,7 @@ public class PlayerDeck extends GroupOfCards {
 
     /**
     * Removes all cards from the player's deck.
+    *
     */
     public void wipePlayerDeck() {
         ArrayList<WarCard> playerDeckCards = this.getPlayerDeck();
@@ -45,19 +52,18 @@ public class PlayerDeck extends GroupOfCards {
 
     }
 
-    //removed the commented out setPlayerDeck(ArrayList<WarCard> playerDeck)
   
     /**
      * Adds a specified WarCard to the player deck.
      *
-     * @param card
+     * @param WaraCard card
      */
     public void addCardToPlayerDeck(WarCard card) {
         super.getCards().add(card);
     }
 
     /**
-     * Returns the card at the specific index
+     * Returns the card at the specific index.
      *
      * @param cardIndex
      * @return the specific war card
@@ -66,6 +72,11 @@ public class PlayerDeck extends GroupOfCards {
         return super.getCards().get(cardIndex);
     }
 
+    /**
+    * Retrieves the size of the player's deck.
+    *
+    * @return size of deck
+    */
     @Override
     public int getSize() {
         return super.getCards().size();
