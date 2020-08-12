6e5c812c873858ccb810339550a271bf4e9a455e
Tibabalase Oludemi <30732454+lafftar@users.noreply.github.com>
2020-08-12 13:57:53 -0400

    Update main.java

--- src/ca/sheridancollege/project/main.java
+++ src/ca/sheridancollege/project/main.java
@@ -1,16 +1,33 @@
-
+/** Runs the Card Game of War
+ * @author Ashley Sun
+ * @author Bryan Acorda
+ * @author Tibabalase Oludemi
+ */
 package ca.sheridancollege.project;
 
-/**
- *
- * @author Bryan Acorda
- * @author TJ
- * @author Ashley Sun
- */
-public class main {
+import java.util.Scanner;
+
+public class Main {
 
     public static void main(String[] args) {
-        WarGame war = new WarGame("War");
-        war.play();
+
+        Scanner input = new Scanner(System.in);
+        WarGame game = new WarGame("War");
+
+        boolean playAgain = false;
+        do {
+            game.play();
+            System.out.print("Do you want to play again? (Yes/No): ");
+            String confirm = input.next();
+
+            if (confirm.equalsIgnoreCase("yes")) {
+                playAgain = true;
+            } else {
+                playAgain = false;
+            }
+        } while (playAgain);
+        System.out.println("Have a good day!");
+        WarGame.sleepForOneSecond();
     }
 }
+
